b891a45fa707039c7af51a7bd2882c6caa474b51
Rita <fedorova.margarita.a@gmail.com>
2025-09-25 23:26:47 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, GetExerciseResponseSchema, ExerciseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, GetExerciseResponseSchema, ExerciseSchema, \
+    UpdateExerciseRequestSchema
 from tools.assertions.base import assert_equal
 
 
@@ -51,3 +52,24 @@ def assert_get_exercise_response(
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
+
+
+def assert_update_exercise_response(
+        update_exercise_request: UpdateExerciseRequestSchema,
+        update_exercise_response: GetExerciseResponseSchema
+):
+    """
+    Проверяет, что ответ на обновление упражнения соответствует данным из запроса.
+
+    :param update_exercise_request: Исходный запрос на обновление упражнения.
+    :param update_exercise_response: Ответ API при обновлении упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(update_exercise_response.exercise.title, update_exercise_request.title,  "title")
+    assert_equal(update_exercise_response.exercise.max_score, update_exercise_request.max_score,  "max_score")
+    assert_equal(update_exercise_response.exercise.min_score, update_exercise_request.min_score,  "min_score")
+    assert_equal(update_exercise_response.exercise.order_index, update_exercise_request.order_index,  "order_index")
+    assert_equal(update_exercise_response.exercise.description, update_exercise_request.description,  "description")
+    assert_equal(
+        update_exercise_response.exercise.estimated_time, update_exercise_request.estimated_time, "estimated_time")
+
